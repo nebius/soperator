72156863fd37f36c703e270a3dd76e07ee999be8
dstaroff <dstaroff@nebius.com>
2024-07-09 17:45:12 +0000

    MSP-2286 Make versioned docker images
    X-Arc-Original-Commit: bcfe97dae47b8c42f0de3fc70b895af0c1884beb

--- images/build_populate_jail.sh
+++ images/build_populate_jail.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-usage() { echo "usage: ${0} [-i][-n][-h]" >&2; exit 1; }
+usage() { echo "usage: ${0} -v <version> [-i] [-n] [-s] [-h]" >&2; exit 1; }
 
-while getopts inh flag
+while getopts v:insh flag
 do
     case "${flag}" in
+        v) version=${OPTARG};;
         i) iopt=-i;;
         n) nopt=-n;;
+        s) stableopt=-s;;
         h) usage;;
         *) usage;;
     esac
@@ -14,7 +16,7 @@ done
 
 mkdir -p outputs
 
-./docker/jail/make_rootfs_tarball.sh -d docker/jail/jail.dockerfile $iopt > outputs/make_rootfs_tarball.log 2>&1
-./build.sh -t populate_jail -d docker/populate_jail/populate_jail.dockerfile $iopt $nopt > outputs/populate_jail.log 2>&1
+./docker/jail/make_rootfs_tarball.sh -d docker/jail/jail.dockerfile ${iopt} > outputs/make_rootfs_tarball.log 2>&1
+./build.sh -d docker/populate_jail/populate_jail.dockerfile -t populate_jail -v "${version}" ${iopt} ${nopt} ${stableopt} > outputs/populate_jail.log 2>&1
 
 echo "Finished: build_populate_jail.sh"
