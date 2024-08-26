ac207c8e3a9fe4d688fae5aa97dc21536312f27f
pavel.sofrony <pavel.sofrony@nebius.com>
2024-08-26 12:09:01 +0000

    MSP-2533 divide operator and terraform release
    X-Arc-Original-Commit: 91d34b6d052b38ad4cec59195534748bf7152559

--- release_all.sh
+++ release_all.sh
@@ -102,31 +102,6 @@ EOF
 ssh -i "$key" "$user"@"$address" "${remote_command}"
 echo "All images are built successfully"
 
-######################################################
-### The code below should be moved to terraform repo
-######################################################
-
-echo "Packing new terraform tarball"
-VERSION=${OPERATOR_IMAGE_TAG} ./terraform/release_terraform.sh -f
-
-echo "Unpacking the terraform tarball"
-version_formatted=$(echo "${OPERATOR_IMAGE_TAG}" | tr '-' '_' | tr '.' '_')
-tarball="slurm_operator_tf_$version_formatted.tar.gz"
-
-pushd ./terraform/releases/unstable
-    VERSION=${OPERATOR_IMAGE_TAG} TARBALL=${tarball} ./unpack_current_version.sh
-popd
-
-GREEN='\033[0;32m'
-RESET='\033[0m'
-
-if [ "$stable" == "1" ]; then
-    mv "terraform/releases/unstable/$tarball" "terraform/releases/stable/"
-    echo -e "${GREEN}Stable version '$OPERATOR_IMAGE_TAG' is successfully released${RESET}"
-else
-    echo -e "${GREEN}Unstable version '$OPERATOR_IMAGE_TAG' is successfully released and unpacked to releases/unstable/${RESET}"
-fi
-
 end_time=$(date +%s)
 duration=$((end_time - start_time))
 
