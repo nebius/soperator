7f073ce461a31700db36fe943fab2658409aa9e8
pavel.sofrony <pavel.sofrony@nebius.com>
2024-08-26 10:00:57 +0000

    uncomment
    X-Arc-Original-Commit: 73f6f75a5a0f3e4824b0d769bf7797c941fbdaa0

--- release_all.sh
+++ release_all.sh
@@ -43,64 +43,64 @@ echo "Version is ${VERSION}"
 echo "Image version is ${IMAGE_VERSION}"
 echo "Operator image tag version is ${OPERATOR_IMAGE_TAG}"
 
-#echo "Updating CRDs & auto-generated code (included in test step) & run tests"
-#make test UNSTABLE="${UNSTABLE}"
-#
-#echo "Uploading images to the build agent"
-#./images/upload_to_build_agent.sh -u "$user" -k "$key"
-#
-#remote_command=$(cat <<EOF
-#set -e
-#set -x
-#
-#echo "Entering /usr/src/prototypes/slurm/${user}"
-#cd "/usr/src/prototypes/slurm/${user}"
-#sudo su -- <<'ENDSSH'
-#
-#echo "Building and pushing container images"
-#
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=worker_slurmd DOCKERFILE=worker/slurmd.dockerfile
-#make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=worker_slurmd
-#
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=controller_slurmctld DOCKERFILE=controller/slurmctld.dockerfile
-#make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=controller_slurmctld
-#
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=login_sshd DOCKERFILE=login/sshd.dockerfile
-#make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=login_sshd
-#
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=munge DOCKERFILE=munge/munge.dockerfile
-#make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=munge
-#
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=nccl_benchmark DOCKERFILE=nccl_benchmark/nccl_benchmark.dockerfile
-#make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=nccl_benchmark
-#echo "Common images were built"
-#
-#echo "Removing previous jail rootfs tar archive"
-#rm -rf images/jail_rootfs.tar
-#
-#echo "Building tarball for jail"
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=jail DOCKERFILE=jail/jail.dockerfile DOCKER_OUTPUT="--output type=tar,dest=jail_rootfs.tar"
-#echo "Built tarball jail_rootfs.tar"
-#
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=populate_jail DOCKERFILE=populate_jail/populate_jail.dockerfile
-#make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=populate_jail
-#
-#echo "Building image of the operator"
-#make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=slurm-operator DOCKERFILE=Dockerfile IMAGE_VERSION="$OPERATOR_IMAGE_TAG"
-#echo "Pushing image of the operator"
-#make docker-push UNSTABLE="${UNSTABLE}" IMAGE_NAME=slurm-operator IMAGE_VERSION="$OPERATOR_IMAGE_TAG"
-#
-#echo "Pushing Helm charts"
-#make release-helm UNSTABLE="${UNSTABLE}" OPERATOR_IMAGE_TAG="${OPERATOR_IMAGE_TAG}"
-#
-#wait
-#
-#ENDSSH
-#EOF
-#)
-#
-#ssh -i "$key" "$user"@"$address" "${remote_command}"
-#echo "All images are built successfully"
+echo "Updating CRDs & auto-generated code (included in test step) & run tests"
+make test UNSTABLE="${UNSTABLE}"
+
+echo "Uploading images to the build agent"
+./images/upload_to_build_agent.sh -u "$user" -k "$key"
+
+remote_command=$(cat <<EOF
+set -e
+set -x
+
+echo "Entering /usr/src/prototypes/slurm/${user}"
+cd "/usr/src/prototypes/slurm/${user}"
+sudo su -- <<'ENDSSH'
+
+echo "Building and pushing container images"
+
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=worker_slurmd DOCKERFILE=worker/slurmd.dockerfile
+make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=worker_slurmd
+
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=controller_slurmctld DOCKERFILE=controller/slurmctld.dockerfile
+make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=controller_slurmctld
+
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=login_sshd DOCKERFILE=login/sshd.dockerfile
+make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=login_sshd
+
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=munge DOCKERFILE=munge/munge.dockerfile
+make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=munge
+
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=nccl_benchmark DOCKERFILE=nccl_benchmark/nccl_benchmark.dockerfile
+make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=nccl_benchmark
+echo "Common images were built"
+
+echo "Removing previous jail rootfs tar archive"
+rm -rf images/jail_rootfs.tar
+
+echo "Building tarball for jail"
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=jail DOCKERFILE=jail/jail.dockerfile DOCKER_OUTPUT="--output type=tar,dest=jail_rootfs.tar"
+echo "Built tarball jail_rootfs.tar"
+
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=populate_jail DOCKERFILE=populate_jail/populate_jail.dockerfile
+make docker-push  UNSTABLE="${UNSTABLE}" IMAGE_NAME=populate_jail
+
+echo "Building image of the operator"
+make docker-build UNSTABLE="${UNSTABLE}" IMAGE_NAME=slurm-operator DOCKERFILE=Dockerfile IMAGE_VERSION="$OPERATOR_IMAGE_TAG"
+echo "Pushing image of the operator"
+make docker-push UNSTABLE="${UNSTABLE}" IMAGE_NAME=slurm-operator IMAGE_VERSION="$OPERATOR_IMAGE_TAG"
+
+echo "Pushing Helm charts"
+make release-helm UNSTABLE="${UNSTABLE}" OPERATOR_IMAGE_TAG="${OPERATOR_IMAGE_TAG}"
+
+wait
+
+ENDSSH
+EOF
+)
+
+ssh -i "$key" "$user"@"$address" "${remote_command}"
+echo "All images are built successfully"
 
 ######################################################
 ### The code below should be moved to terraform repo
