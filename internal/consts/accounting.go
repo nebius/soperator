ee1fcf271c633e35b4dfd4d82e84465596aefd5c
Uburro <grigorii.rochev@nebius.com>
2024-09-12 11:27:50 +0200

    MSP-2788: del DbdPort

--- internal/consts/accounting.go
+++ internal/consts/accounting.go
@@ -3,7 +3,6 @@ package consts
 const (
 	SecretSlurmdbdConfigs       = "slurmdbd-configs"
 	HostnameAccounting          = "accounting"
-	DbdPort                     = "6819"
 	DefaultAccountingPort       = 6819
 	SlurmdbdPidFile             = "/var/run/slurmdbd.pid"
 	EmptyDirAccountingName      = "accounting"
