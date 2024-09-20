369af32ac16d981c4ba34dab30eecf516521fe19
Uburro <grigorii.rochev@nebius.com>
2024-09-20 17:43:02 +0200

    add innodb_buffer_pool_size

--- internal/consts/mariadb.go
+++ internal/consts/mariadb.go
@@ -13,7 +13,7 @@ const (
 bind-address=*
 default_storage_engine=InnoDB
 innodb_default_row_format=DYNAMIC
-innodb_buffer_pool_size=32768M
+innodb_buffer_pool_size=4096M
 innodb_log_file_size=64M
 innodb_lock_wait_timeout=900
 max_allowed_packet=16M`
