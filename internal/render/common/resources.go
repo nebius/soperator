1bf0ec35664a5eefa1fdeed6493890f006863768
Uburro <grigorii.rochev@nebius.com>
2024-11-04 15:51:19 +0100

    fix comment

--- internal/render/common/resources.go
+++ internal/render/common/resources.go
@@ -4,10 +4,10 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+// CopyNonCPUResources returns a copy of corev1.ResourceList but excludes CPU from it.
+// This is useful for getting resource limits from resource requests for K8s containers.
+// They usually should be identical for everything except CPU, because setting CPU limits may lead to throttling by CFS scheduler in Linux.
 func CopyNonCPUResources(resourceList corev1.ResourceList) corev1.ResourceList {
-	// Create a copy of all the resources except CPU.
-	// This is useful for setting limits on resources other than CPU and prevents throttling of cpu.
-	// Also, it required value: Limit must be set for non overcommitable resource
 	limits := corev1.ResourceList{}
 	for resourceName, quantity := range resourceList {
 		if resourceName != corev1.ResourceCPU {
