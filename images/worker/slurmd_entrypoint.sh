f77c20094d576db74ffa65de0db44bc7fb72bffd
Pavel Sofronii <pavel.sofrony@nebius.com>
2024-12-09 16:43:28 +0100

    set compute instance name as slurm InstanceId

--- images/worker/slurmd_entrypoint.sh
+++ images/worker/slurmd_entrypoint.sh
@@ -95,6 +95,7 @@ echo "Start slurmd daemon"
 exec /usr/sbin/slurmd \
   -D \
   -Z \
+  --instance-id "${INSTANCE_ID}" \
   --conf \
   "NodeHostname=${K8S_POD_NAME} NodeAddr=${K8S_POD_NAME}.${K8S_SERVICE_NAME}.${K8S_POD_NAMESPACE}.svc.cluster.local RealMemory=${SLURM_REAL_MEMORY} Gres=${GRES}" \
   2>&1 | tee >(multilog s100000000 n5 /var/log/slurm/multilog)
