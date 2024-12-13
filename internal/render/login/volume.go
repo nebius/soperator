960c57b88dea5424edd87261e0912df401bd909f
Grigorii Rochev <31252905+Uburro@users.noreply.github.com>
2024-12-13 16:25:51 +0100

    MSP-3578: add sshd to worker (#248)
    * MSP-3578: add sshd to worker

--- internal/render/login/volume.go
+++ internal/render/login/volume.go
@@ -2,11 +2,9 @@ package login
 
 import (
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/utils/ptr"
 
 	slurmv1 "nebius.ai/slurm-operator/api/v1"
 	"nebius.ai/slurm-operator/internal/consts"
-	"nebius.ai/slurm-operator/internal/naming"
 	"nebius.ai/slurm-operator/internal/render/common"
 	"nebius.ai/slurm-operator/internal/values"
 )
@@ -22,9 +20,9 @@ func renderVolumesAndClaimTemplateSpecs(
 		common.RenderVolumeMungeKey(clusterName),
 		common.RenderVolumeMungeSocket(),
 		common.RenderVolumeSecurityLimits(clusterName, consts.ComponentTypeLogin),
-		renderVolumeSshdKeys(secrets.SshdKeysName),
-		renderVolumeSshConfigs(clusterName),
-		renderVolumeSshRootKeys(clusterName),
+		common.RenderVolumeSshdKeys(secrets.SshdKeysName),
+		common.RenderVolumeSshConfigs(clusterName),
+		common.RenderVolumeSshRootKeys(clusterName),
 	}
 
 	// Jail could be specified by template spec or by volume source name
@@ -52,120 +50,3 @@ func renderVolumesAndClaimTemplateSpecs(
 
 	return volumes, pvcTemplateSpecs, nil
 }
-
-// region SSH
-
-// region configs
-
-// renderVolumeSshConfigs renders [corev1.Volume] containing SSH configs contents
-func renderVolumeSshConfigs(clusterName string) corev1.Volume {
-	return corev1.Volume{
-		Name: consts.VolumeNameSSHConfigs,
-		VolumeSource: corev1.VolumeSource{
-			ConfigMap: &corev1.ConfigMapVolumeSource{
-				LocalObjectReference: corev1.LocalObjectReference{
-					Name: naming.BuildConfigMapSSHConfigsName(clusterName),
-				},
-			},
-		},
-	}
-}
-
-// renderVolumeMountSshConfigs renders [corev1.VolumeMount] defining the mounting path for SSH configs
-func renderVolumeMountSshConfigs() corev1.VolumeMount {
-	return corev1.VolumeMount{
-		Name:      consts.VolumeNameSSHConfigs,
-		MountPath: consts.VolumeMountPathSSHConfigs,
-		ReadOnly:  true,
-	}
-}
-
-// endregion configs
-
-// region root keys
-
-// renderVolumeSshRootKeys renders [corev1.Volume] containing SSH root keys contents
-func renderVolumeSshRootKeys(clusterName string) corev1.Volume {
-	return corev1.Volume{
-		Name: consts.VolumeNameSSHRootKeys,
-		VolumeSource: corev1.VolumeSource{
-			ConfigMap: &corev1.ConfigMapVolumeSource{
-				LocalObjectReference: corev1.LocalObjectReference{
-					Name: naming.BuildConfigMapSshRootPublicKeysName(clusterName),
-				},
-				DefaultMode: ptr.To(int32(384)),
-			},
-		},
-	}
-}
-
-// renderVolumeMountSshRootKeys renders [corev1.VolumeMount] defining the mounting path for SSH root keys
-func renderVolumeMountSshRootKeys() corev1.VolumeMount {
-	return corev1.VolumeMount{
-		Name:      consts.VolumeNameSSHRootKeys,
-		MountPath: consts.VolumeMountPathSSHRootKeys,
-		SubPath:   consts.VolumeMountSubPathSSHRootKeys,
-		ReadOnly:  true,
-	}
-}
-
-// endregion root keys
-
-// region sshd keys
-
-// renderVolumeSshdKeys renders [corev1.Volume] containing SSHD key pairs
-func renderVolumeSshdKeys(sshdKeysSecretName string) corev1.Volume {
-	return corev1.Volume{
-		Name: consts.VolumeNameSSHDKeys,
-		VolumeSource: corev1.VolumeSource{
-			Secret: &corev1.SecretVolumeSource{
-				SecretName: sshdKeysSecretName,
-				Items: []corev1.KeyToPath{
-					{
-						Key:  consts.SecretSshdECDSAKeyName,
-						Path: consts.SecretSshdECDSAKeyName,
-						Mode: ptr.To(consts.SecretSshdKeysPrivateFileMode),
-					},
-					{
-						Key:  consts.SecretSshdECDSAPubKeyName,
-						Path: consts.SecretSshdECDSAPubKeyName,
-						Mode: ptr.To(consts.SecretSshdKeysPublicFileMode),
-					},
-					{
-						Key:  consts.SecretSshdECDSA25519KeyName,
-						Path: consts.SecretSshdECDSA25519KeyName,
-						Mode: ptr.To(consts.SecretSshdKeysPrivateFileMode),
-					},
-					{
-						Key:  consts.SecretSshdECDSA25519PubKeyName,
-						Path: consts.SecretSshdECDSA25519PubKeyName,
-						Mode: ptr.To(consts.SecretSshdKeysPublicFileMode),
-					},
-					{
-						Key:  consts.SecretSshdRSAKeyName,
-						Path: consts.SecretSshdRSAKeyName,
-						Mode: ptr.To(consts.SecretSshdKeysPrivateFileMode),
-					},
-					{
-						Key:  consts.SecretSshdRSAPubKeyName,
-						Path: consts.SecretSshdRSAPubKeyName,
-						Mode: ptr.To(consts.SecretSshdKeysPublicFileMode),
-					},
-				},
-			},
-		},
-	}
-}
-
-// renderVolumeMountSshdKeys renders [corev1.VolumeMount] defining the mounting path for SSHD key pairs
-func renderVolumeMountSshdKeys() corev1.VolumeMount {
-	return corev1.VolumeMount{
-		Name:      consts.VolumeNameSSHDKeys,
-		MountPath: consts.VolumeMountPathSSHDKeys,
-		ReadOnly:  true,
-	}
-}
-
-// endregion sshd keys
-
-// endregion SSH
