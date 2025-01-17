5b5d2bae4d1e0cacf5e5017a3b319d816b25a8aa
Pavel Sofronii <pavel.sofrony@nebius.com>
2025-01-17 12:17:44 +0100

    Move gpubenchmark binary to the worker container and link it to the jail

--- images/worker/supervisord_entrypoint.sh
+++ images/worker/supervisord_entrypoint.sh
@@ -33,6 +33,9 @@ for file in /mnt/slurm-configs/*; do
     touch "/etc/slurm/$filename" && mount --bind "$file" "/etc/slurm/$filename"
 done
 
+echo "Link gpubenchmark from container ot jail"
+ln -s /usr/bin/gpubench /mnt/jail/usr/bin/gpubench
+
 echo "Make ulimits as big as possible"
 set_ulimit() {
     local limit_option=$1
