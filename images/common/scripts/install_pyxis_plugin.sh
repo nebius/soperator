f3c4a8b82fe17bec212fd47fd20bc8fd1d9668d4
Dzmitry Amialiusik <amialiusik@nebius.com>
2025-02-05 11:00:10 +0100

    MSP-3890: update pyxis config

--- images/common/scripts/install_pyxis_plugin.sh
+++ images/common/scripts/install_pyxis_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PYXIS_VERSION=0.20.0
+PYXIS_VERSION=0.21.0
 
 wget -q -P /tmp https://github.com/nebius/slurm-deb-packages/releases/download/12.4.1-jammy-slurm24.05.5/nvslurm-plugin-pyxis_"$PYXIS_VERSION"-1_amd64.deb
 dpkg -i /tmp/nvslurm-plugin-pyxis_"$PYXIS_VERSION"-1_amd64.deb
