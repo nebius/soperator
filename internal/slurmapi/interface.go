ea50d4024b1d29f2212a692c57f246608c785d97
Dzmitry Amialiusik <amialiusik@nebius.com>
2025-02-13 23:41:23 +0100

    Add slurmapi package

---
+++ internal/slurmapi/interface.go
@@ -0,0 +1,13 @@
+package slurmapi
+
+import (
+	"context"
+
+	api "github.com/SlinkyProject/slurm-client/api/v0041"
+)
+
+type Client interface {
+	api.ClientWithResponsesInterface
+	ListNodes(ctx context.Context) ([]Node, error)
+	GetNode(ctx context.Context, nodeName string) (Node, error)
+}
