33c60a68344cde554e56d8bb082fdb3fd5fb272d
Dzmitry Amialiusik <amialiusik@nebius.com>
2025-02-24 12:55:54 +0100

    add node delete to rbac

--- internal/soperatorchecks/reconcile.go
+++ internal/soperatorchecks/reconcile.go
@@ -25,7 +25,8 @@ import (
 )
 
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;delete;update
-//+kubebuilder:rbac:groups=core,resources=nodes;secrets,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch;delete;update
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;update
 //+kubebuilder:rbac:groups=core,resources=nodes/status,verbs=get;update;patch;watch;list
 
 var (
