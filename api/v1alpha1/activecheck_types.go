b39bb74f27986d782d157911329e5c83d5acd88c
Pavel Sofronii <pavel.sofrony@nebius.com>
2025-03-18 15:12:20 +0100

    Change soperatorconfig CRD for k8sJob

--- api/v1alpha1/activecheck_types.go
+++ api/v1alpha1/activecheck_types.go
@@ -1,27 +1,20 @@
-/*
-Copyright 2024 Nebius B.V.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // ActiveCheckSpec defines the desired state of ActiveCheck.
 type ActiveCheckSpec struct {
+	// Name defines the name of k8s cronJob
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+
+	// SlurmClusterRefName defines the name of Slurm Cluster
+	// +kubebuilder:validation:Required
+	SlurmClusterRefName string `json:"slurmClusterRefName"`
+
 	// Schedule defines the CronJob schedule.
 	// By default, every year - at 00:00 on day-of-month 1 in January
 	// +kubebuilder:validation:Optional
@@ -33,17 +26,59 @@ type ActiveCheckSpec struct {
 	// +kubebuilder:default=true
 	Suspend bool `json:"suspend,omitempty"`
 
+	// ActiveDeadlineSeconds defines the CronJob timeout in seconds
+	//
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=1800
+	ActiveDeadlineSeconds int64 `json:"activeDeadlineSeconds,omitempty"`
+
+	// SuccessfulJobsHistoryLimit defines the number of successful finished jobs to retain
+	//
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=3
+	SuccessfulJobsHistoryLimit int32 `json:"successfulJobsHistoryLimit,omitempty"`
+
+	// FailedJobsHistoryLimit defines the number of failed finished jobs to retain
+	//
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=16
+	FailedJobsHistoryLimit int32 `json:"failedJobsHistoryLimit,omitempty"`
+
 	// RunAfterCreation specifies whether the job should run immediately after the CronJob is created.
 	// +kubebuilder:default=true
 	RunAfterCreation bool `json:"runAfterCreation,omitempty"`
+
+	// NodeSelector defines the desired selector for the K8s nodes to place Slurm workers on
+	//
+	// NOTE: NodeSelector could not be set if Affinity is specified
+	//
+	// +kubebuilder:validation:Optional
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// Affinity defines the desired affinity for the K8s nodes to place Slurm workers on
+	// NOTE: Affinity could not be set if NodeSelector is specified
+	//
+	// +kubebuilder:validation:Optional
+	Affinity *corev1.Affinity `json:"affinity,omitempty"`
+
+	// Tolerations define the desired tolerations for the K8s nodes to place Slurm workers on
+	//
+	// +kubebuilder:validation:Optional
+	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
 
 	// PodTemplateNameRef points to a PodTemplate that contains the pod configuration. Use it to override the default settings.
 	// +kubebuilder:validation:Optional
 	PodTemplateNameRef *string `json:"podTemplateNameRef,omitempty"`
 
-	// SpecNCCLTests defines settings for specific type of the check
+	// K8sJobSpec defines options for k8s cronjob
 	// +kubebuilder:validation:Optional
-	SpecNCCLTests SpecNCCLTests `json:"specNCCLTests,omitempty"`
+	K8sJobSpec K8sJobSpec `json:"k8sJobSpec,omitempty"`
+
+	// CheckType defines the type of the check
+	// +kubebuilder:validation:Enum=k8sJob;slurmJob
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default="k8sJob"
+	CheckType string `json:"checkType,omitempty"`
 
 	// Reactions defines reaction on specific check
 	// +kubebuilder:validation:Optional
@@ -52,38 +87,29 @@ type ActiveCheckSpec struct {
 
 type Reactions struct {
 	// SetCondition enabling setting condition to the k8s node
+	// +kubebuilder:validation:Optional
 	SetCondition bool `json:"setCondition,omitempty"`
 
 	// DrainSlurmNode enabling slurm node draining if check failed
+	// +kubebuilder:validation:Optional
 	DrainSlurmNode bool `json:"DrainSlurmNode,omitempty"`
 }
 
-type SpecNCCLTests struct {
-	// Name + CheckType defines the name of k8s cronJob
+type K8sJobSpec struct {
+	Image   string   `json:"image,omitempty"`
+	Command []string `json:"command,omitempty"`
+	// ScriptRefName name of configMap with custom script
 	// +kubebuilder:validation:Optional
-	// +kubebuilder:default=nccl-test
-	Name string `json:"name,omitempty"`
-
-	// Priority defines the priority of k8s cronJob
-	// +kubebuilder:validation:Optional
-	// +kubebuilder:default=10
-	Priority int32 `json:"priority,omitempty"`
-
-	// CheckType defines the name of the binary that will be executed during tests.
-	// +kubebuilder:validation:Enum=all_gather_perf;all_reduce_perf;alltoall_perf;broadcast_perf;gather_perf;hypercube_perf;reduce_perf;reduce_scatter_perf;scatter_perf;sendrecv_perf;all_gather_perf_mpi;all_reduce_perf_mpi;alltoall_perf_mpi;broadcast_perf_mpi;gather_perf_mpi;hypercube_perf_mpi;reduce_perf_mpi;reduce_scatter_perf_mpi;scatter_perf_mpi;sendrecv_perf_mpi
-	// +kubebuilder:validation:Optional
-	// +kubebuilder:default=all_reduce_perf
-	CheckType string `json:"checkType,omitempty"`
-
-	// Args defines raw params string
-	// +kubebuilder:validation:Optional
-	Args string `json:"args,omitempty"`
+	ScriptRefName *string              `json:"scriptRefName,omitempty"`
+	Args          []string             `json:"args,omitempty"`
+	Env           []corev1.EnvVar      `json:"env,omitempty"`
+	VolumeMounts  []corev1.VolumeMount `json:"volumeMounts,omitempty"`
+	Volumes       []corev1.Volume      `json:"volumes,omitempty"`
 }
 
 // ActiveCheckStatus defines the observed state of ActiveCheck.
 type ActiveCheckStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	StatusMetadata `json:",inline"`
 }
 
 // +kubebuilder:object:root=true
