0248252f03f051a724d850cc15206b64c9c05375
Dzmitry Amialiusik <amialiusik@nebius.com>
2025-03-25 16:50:44 +0100

    terraform apply/destroy scenario

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,11 +1,164 @@
+//go:build e2e
+
 package e2e
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/stretchr/testify/require"
+	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
 
-// TODO: Implement E2E tests
-// it needs to be implemented for kubebuilder create webhook --group core --version v1 --kind Secret --programmatic-validation
-func TestE2E(t *testing.T) {
-	t.Log("E2E test placeholder")
+type testConfig struct {
+	PathToTerraform  string   `split_words:"true" required:"true"` // PATH_TO_TERRAFORM
+	InfinibandFabric string   `split_words:"true" required:"true"` // INFINIBAND_FABRIC
+	SSHKeys          []string `split_words:"true" required:"true"` // SSH_KEYS
+}
+
+func TestTerraform(t *testing.T) {
+	var cfg testConfig
+
+	err := envconfig.Process("", &cfg)
+	require.NoError(t, err)
+
+	tfVars := readTFVars(t, fmt.Sprintf("%s/terraform.tfvars", cfg.PathToTerraform))
+	tfVars = overrideTestValues(tfVars, cfg)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: cfg.PathToTerraform,
+		Vars:         tfVars,
+		RetryableTerraformErrors: map[string]string{
+			"Context deadline exceeded": "retry on context deadline exceeded",
+		},
+		MaxRetries: 5,
+	}
+
+	terraform.Init(t, terraformOptions)
+	terraform.WorkspaceSelectOrNew(t, terraformOptions, "e2e-test")
+	terraform.Destroy(t, terraformOptions)
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.Apply(t, terraformOptions)
+}
+
+func readTFVars(t *testing.T, tfVarsFilename string) map[string]interface{} {
+	tfVarsFile, err := os.ReadFile(tfVarsFilename)
+	require.NoError(t, err)
+
+	hclFile, diags := hclsyntax.ParseConfig(tfVarsFile, tfVarsFilename, hcl.InitialPos)
+	require.False(t, diags.HasErrors(), diags.Error())
+
+	attrs, diags := hclFile.Body.JustAttributes()
+	require.False(t, diags.HasErrors(), diags.Error())
+
+	values := make(map[string]json.RawMessage, len(attrs))
+	for name, attr := range attrs {
+		value, diags := attr.Expr.Value(nil)
+		require.False(t, diags.HasErrors(), diags.Error())
+
+		buf, err := ctyjson.Marshal(value, value.Type())
+		require.NoError(t, err)
+
+		values[name] = json.RawMessage(buf)
+	}
+
+	output, err := json.MarshalIndent(values, "", "  ")
+	require.NoError(t, err)
+
+	var varsMap map[string]interface{}
+
+	err = json.Unmarshal(output, &varsMap)
+	require.NoError(t, err)
+
+	return varsMap
+}
+
+func overrideTestValues(tfVars map[string]interface{}, cfg testConfig) map[string]interface{} {
+	// company_name = "e2e-test"
+	tfVars["company_name"] = "e2e-test"
+
+	// filestore_jail = {
+	//   spec = {
+	//     size_gibibytes       = 2048
+	//     block_size_kibibytes = 4
+	//   }
+	// }
+	tfVars["filestore_jail"] = []interface{}{
+		map[string]interface{}{
+			"spec": map[string]interface{}{
+				"size_gibibytes":       2048,
+				"block_size_kibibytes": 4,
+			},
+		},
+	}
+
+	//  filestore_jail_submounts = [{
+	//    name       = "data"
+	//    mount_path = "/mnt/data"
+	//    spec = {
+	//    size_gibibytes       = 2048
+	//    block_size_kibibytes = 4
+	//    }
+	//  }]
+	tfVars["filestore_jail_submounts"] = []interface{}{
+		map[string]interface{}{
+			"name":       "data",
+			"mount_path": "/mnt/data",
+			"spec": map[string]interface{}{
+				"size_gibibytes":       2048,
+				"block_size_kibibytes": 4,
+			},
+		},
+	}
+
+	// slurm_nodeset_workers = [{
+	// 	 size                    = 2
+	// 	 nodes_per_nodegroup     = 1
+	// 	 max_unavailable_percent = 50
+	// 	 resource = {
+	// 	   platform = "gpu-h100-sxm"
+	// 	   preset   = "8gpu-128vcpu-1600gb"
+	// 	 }
+	// 	 boot_disk = {
+	// 	   type                 = "NETWORK_SSD"
+	// 	   size_gibibytes       = 2048
+	// 	   block_size_kibibytes = 4
+	// 	 }
+	// 	 gpu_cluster = {
+	// 	   infiniband_fabric = ""
+	// 	 }
+	// }]
+	tfVars["slurm_nodeset_workers"] = []interface{}{
+		map[string]interface{}{
+			"size":                    2,
+			"nodes_per_nodegroup":     1,
+			"max_unavailable_percent": 50,
+			"resource": map[string]interface{}{
+				"platform": "gpu-h100-sxm",
+				"preset":   "8gpu-128vcpu-1600gb",
+			},
+			"boot_disk": map[string]interface{}{
+				"type":                 "NETWORK_SSD",
+				"size_gibibytes":       2048,
+				"block_size_kibibytes": 4,
+			},
+			"gpu_cluster": map[string]interface{}{
+				"infiniband_fabric": cfg.InfinibandFabric,
+			},
+		},
+	}
+
+	// slurm_login_ssh_root_public_keys = [
+	//   "ssh-rsa somekey==",
+	// ]
+	tfVars["slurm_login_ssh_root_public_keys"] = cfg.SSHKeys
+
+	return tfVars
 }
