68c4f495d179a8f93dbf1a0d9efe54133ac3a6c5
Barys Serankou <chessprofessor@nebius.com>
2025-05-02 10:56:25 +0200

    Remove unnecessary dependencies + update one_job

--- images/k8s_job/k8s_job_entrypoint.sh
+++ images/k8s_job/k8s_job_entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -e # Exit immediately if any command returns a non-zero error code
 
-echo "Start sshd daemon"
-/usr/sbin/sshd -D -e -f /mnt/ssh-configs/sshd_config
-sleep 1
-
 # TODO: Using example job just to test image now. Fix it later.
 echo "Submitting Slurm job..."
 SLURM_OUTPUT=$(sbatch --parsable --wrap="echo Hello from Slurm")
