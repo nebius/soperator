e6c720f53046bfe340e19b482a24cddf3f5000eb
Pavel Sofronii <pavel.sofrony@nebius.com>
2025-05-19 15:06:34 +0200

    Install deb packages from Nebius deb repo for login image with slurm 24.05.7

--- images/common/scripts/install_enroot.sh
+++ images/common/scripts/install_enroot.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Download enroot
-curl -fSsL -o /tmp/enroot_3.5.0-1_amd64.deb https://github.com/NVIDIA/enroot/releases/download/v3.5.0/enroot_3.5.0-1_amd64.deb
-curl -fSsL -o /tmp/enroot+caps_3.5.0-1_amd64.deb https://github.com/NVIDIA/enroot/releases/download/v3.5.0/enroot+caps_3.5.0-1_amd64.deb
+ENROOT_VERSION=3.5.0
 
-# Install downloaded packages & rm them
-apt install -y /tmp/*.deb
-rm -rf /tmp/*.deb
-apt clean
+apt-get update
+apt -y install enroot=${ENROOT_VERSION}-1 enroot+caps=${ENROOT_VERSION}-1
+apt-get clean
+rm -rf /var/lib/apt/lists/*
 
 # Add an extra hook that sets env vars for PyTorch
-curl -fSsL -o /etc/enroot/hooks.d/50-slurm-pytorch.sh https://raw.githubusercontent.com/NVIDIA/enroot/refs/tags/v3.5.0/conf/hooks/extra/50-slurm-pytorch.sh
+curl -fSsL -o /etc/enroot/hooks.d/50-slurm-pytorch.sh "https://raw.githubusercontent.com/NVIDIA/enroot/refs/tags/v${ENROOT_VERSION}/conf/hooks/extra/50-slurm-pytorch.sh"
 chmod +x /etc/enroot/hooks.d/50-slurm-pytorch.sh
 
 # Prepare env for running enroot
