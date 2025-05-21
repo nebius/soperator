7c48a191926b0a2669d865c1b5bdb2745654acdf
Ali Sattari <ali.sattari@nebius.com>
2025-05-21 14:24:51 +0200

    Adding DCGM Exporter to helm chart and values (#873)
    * Adding DCGM Exporter to helm chart and values
    * map/unmap jobs in prolog/epilog
    * warning for future self

--- fluxcd/environment/nebius-cloud/base/custom-configmaps-soperator/epilog.sh
+++ fluxcd/environment/nebius-cloud/base/custom-configmaps-soperator/epilog.sh
@@ -20,3 +20,22 @@ if [ -n "$SLURM_JOB_GPUS" ]; then
     exit 0
     fi
 fi
+
+echo "Unmap the Slurm job with DCGM metrics"
+# set in hpcJobMapDir in soperator/helm/soperator-fluxcd/values.yaml
+#   and dcgm_job_map_dir in nebius-solution-library/soperator/modules/slurm/variables.tf
+#   check those before changing it here
+metrics_dir="/var/run/nebius/slurm"
+
+if [[ -z "${CUDA_VISIBLE_DEVICES:-}" ]]; then
+    echo "No GPU devices are requested by user" >&2
+    exit 0
+fi
+
+IFS=',' read -ra cuda_devs <<< "$CUDA_VISIBLE_DEVICES"
+
+for gpu_id in "${cuda_devs[@]}"; do
+    [[ -z "$gpu_id" ]] && continue
+    echo "Removing $metrics_dir/${gpu_id:-99}"
+    rm -f "${METRICS_DIR:-}/${gpu_id:-99}" || echo "Unable to remove file ${METRICS_DIR:-}/${gpu_id:-99}"
+done
