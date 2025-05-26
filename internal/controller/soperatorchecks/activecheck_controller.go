bd7b38a7086319473e856ecc40c21dc0efa97de2
iamrajiv <rajivperfect007@gmail.com>
2025-05-26 21:31:04 +0530

    add missing

--- internal/controller/soperatorchecks/activecheck_controller.go
+++ internal/controller/soperatorchecks/activecheck_controller.go
@@ -9,6 +9,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
