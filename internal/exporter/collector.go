b9e5479aa347d0664d8373602167900fcd206d5a
Viacheslav Ezhkin <yoprst@nebius.com>
2025-06-02 18:22:09 +0200

    slurm_cluster_info -> soperator_cluster_info

--- internal/exporter/collector.go
+++ internal/exporter/collector.go
@@ -13,28 +13,28 @@ import (
 type MetricsCollector struct {
 	slurmAPIClient slurmapi.Client
 
-	clusterInfo *prometheus.Desc
-	nodeInfo    *prometheus.Desc
-	jobInfo     *prometheus.Desc
-	jobNode     *prometheus.Desc
+	sopClusterInfo *prometheus.Desc
+	nodeInfo       *prometheus.Desc
+	jobInfo        *prometheus.Desc
+	jobNode        *prometheus.Desc
 }
 
 // NewMetricsCollector creates a new MetricsCollector
 func NewMetricsCollector(slurmAPIClient slurmapi.Client, soperatorVersion string) *MetricsCollector {
-	clusterInfoConstLabels := prometheus.Labels{"soperator_version": soperatorVersion}
+	sopClusterInfoConstLabels := prometheus.Labels{"soperator_version": soperatorVersion}
 	return &MetricsCollector{
 		slurmAPIClient: slurmAPIClient,
 
-		clusterInfo: prometheus.NewDesc("slurm_cluster_info", "Slurm cluster information", []string{}, clusterInfoConstLabels),
-		nodeInfo:    prometheus.NewDesc("slurm_node_info", "Slurm node detail information", []string{}, nil),
-		jobInfo:     prometheus.NewDesc("slurm_job_info", "Slurm job detail information", []string{}, nil),
-		jobNode:     prometheus.NewDesc("slurm_node_job", "Slurm job node information", []string{"job_id", "node_name"}, nil),
+		sopClusterInfo: prometheus.NewDesc("soperator_cluster_info", "Soperator cluster information", []string{}, sopClusterInfoConstLabels),
+		nodeInfo:       prometheus.NewDesc("slurm_node_info", "Slurm node detail information", []string{}, nil),
+		jobInfo:        prometheus.NewDesc("slurm_job_info", "Slurm job detail information", []string{}, nil),
+		jobNode:        prometheus.NewDesc("slurm_node_job", "Slurm job node information", []string{"job_id", "node_name"}, nil),
 	}
 }
 
 // Describe implements the prometheus.Collector interface
 func (c *MetricsCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.clusterInfo
+	ch <- c.sopClusterInfo
 	ch <- c.nodeInfo
 	ch <- c.jobInfo
 	ch <- c.jobNode
@@ -42,7 +42,7 @@ func (c *MetricsCollector) Describe(ch chan<- *prometheus.Desc) {
 
 // Collect implements the prometheus.Collector interface
 func (c *MetricsCollector) Collect(ch chan<- prometheus.Metric) {
-	ch <- prometheus.MustNewConstMetric(c.clusterInfo, prometheus.GaugeValue, 1)
+	ch <- prometheus.MustNewConstMetric(c.sopClusterInfo, prometheus.GaugeValue, 1)
 
 	ctx := context.Background()
 	logger := log.FromContext(ctx).WithName(ControllerName)
