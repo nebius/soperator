837ac8ddcbb481939742f5f94b9d8c83e20da507
Barys Serankou <chessprofessor@nebius.com>
2025-06-17 15:04:33 +0200

    Small fix

--- internal/consts/activecheck.go
+++ internal/consts/activecheck.go
@@ -3,6 +3,7 @@ package consts
 const (
 	ActiveCheckFinalizer                    = "slurm.nebius.ai/activecheck-finalizer"
 	SlurmNodeReasonActiveCheckFailedUnknown = "Soperator activecheck unknown: node drain process"
-	ActiveCheckEachWorkerJobArrayEnv        = "EACH_WORKER_JOB_ARRAY"
-	ActiveCheckNameEnv                      = "ACTIVE_CHECK_NAME"
+
+	ActiveCheckEachWorkerJobArrayEnv = "EACH_WORKER_JOB_ARRAY"
+	ActiveCheckNameEnv               = "ACTIVE_CHECK_NAME"
 )
