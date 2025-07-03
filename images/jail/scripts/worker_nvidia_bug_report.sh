c753e64b1fdf5443998332a17ee9de6009c3616d
rdjjke <rdjjke@gmail.com>
2025-07-03 12:09:55 +0200

    NOTIC: Add nvidia-bug-report utility script

---
+++ images/jail/scripts/worker_nvidia_bug_report.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e # Exit immediately if any command returns a non-zero error code
+
+usage() {
+    echo "Get nvidia bug report from a worker node." >&2
+    echo "" >&2
+    echo "usage: ${0} [-w worker_name] [-i instance_id] [-h]" >&2
+    echo "       (either -w or -i must be set)"
+    exit 1
+}
+
+while getopts w:i:h flag
+do
+    case "${flag}" in
+        w) worker_name=${OPTARG};;
+        i) instance_id=${OPTARG};;
+        h) usage;;
+        *) usage;;
+    esac
+done
+
+if [ -z "$worker_name" ] && [ -z "$instance_id" ]; then
+    usage
+fi
+
+if [ -z "$worker_name" ] && [ -n "$instance_id" ]; then
+    worker_name=$(scontrol show nodes --json | jq -r ".nodes[] | select(.instance_id == \"${instance_id}\") | .name")
+fi
+
+ssh -t "${worker_name}" bash -s <<'EOF'
+  sudo chroot /run/nvidia/driver /usr/bin/nvidia-bug-report.sh
+  sudo mv /run/nvidia/driver/nvidia-bug-report.log.gz /tmp/
+EOF
+
+scp "${worker_name}":/tmp/nvidia-bug-report.log.gz "$(pwd)/${worker_name}-nvidia-bug-report.log.gz"
