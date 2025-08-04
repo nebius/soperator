c0be573527ca064732001447360bf99820768d7a
Barys Serankou <chessprofessor@nebius.com>
2025-08-04 16:18:14 +0200

    Add memory latency and bandwidth Active Checks

---
+++ helm/soperator-activechecks/scripts/mem-bw.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --deadline="now+6hours"
+#SBATCH --time=00:10:00
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=8
+#SBATCH --exclusive
+
+platform=""
+gpus_on_node=$(nvidia-smi --query-gpu=name --format=csv,noheader | sort | uniq -c)
+if [[ "${gpus_on_node}" == *"8 NVIDIA H100"* ]]; then
+  platform="8xH100"
+elif [[ "${gpus_on_node}" == *"8 NVIDIA H200"* ]]; then
+  platform="8xH200"
+elif [[ "${gpus_on_node}" == *"8 NVIDIA B200"* ]]; then
+  platform="8xB200"
+else
+  echo "Unsupported platform"
+  exit 0
+fi
+
+echo "Platform found: $platform"
+echo "Running mem_bw check on $(hostname)..."
+HC_OUTPUT=$(srun --cpu-bind=verbose,cores bash -c "health-checker run -e soperator -p $platform -n mem_bw --json-log")
+HC_EXIT_CODE=$?
+
+echo "Health checker output: $HC_OUTPUT"
+echo "Health checker job step exit code: $HC_EXIT_CODE"
+HC_STATUS=$(echo "$HC_OUTPUT" | awk '/^\s*{/,/^\s*}/' | jq -r '.status')
+
+echo "Health checker status: $HC_STATUS"
+if [[ "$HC_STATUS" == "ERROR" && $HC_EXIT_CODE -eq 1 ]]; then
+  echo "Health-checker reported status=ERROR and exited with non-zero status."
+  exit 1
+else
+  echo "Health-checker passed or returned non-ERROR status."
+  exit 0
+fi
