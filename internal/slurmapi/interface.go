e827f3133909216d336f15612d3a80794b26e3ee
Meena <meena@nebius.com>
2025-08-05 17:27:32 +0200

    slurm reservation for suspecious nodes 2

--- internal/slurmapi/interface.go
+++ internal/slurmapi/interface.go
@@ -13,4 +13,5 @@ type Client interface {
 	GetJobsByID(ctx context.Context, jobID string) ([]Job, error)
 	ListJobs(ctx context.Context) ([]Job, error)
 	GetDiag(ctx context.Context) (*api.V0041OpenapiDiagResp, error)
+	PostMaintenanceReservation(ctx context.Context, name string, nodeList []string) error
 }
