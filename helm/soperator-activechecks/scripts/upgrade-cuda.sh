057b0b2e5dfdba985613b3270b3fcd902803e64d
Dzmitry Amialiusik <amialiusik@nebius.com>
2025-08-06 14:27:37 +0200

    add health-checker upgrade

--- helm/soperator-activechecks/scripts/upgrade-cuda.sh
+++ helm/soperator-activechecks/scripts/upgrade-cuda.sh
@@ -8,7 +8,8 @@ echo "Upgrading cuda"
 # other node become unavailable for new SSH connections for around 10 seconds.
 retry -d 2 -t 10 -- ssh -i /mnt/jail/opt/soperator-home/soperatorchecks/.ssh/soperatorchecks_id_ecdsa \
     -o StrictHostKeyChecking=no \
-    soperatorchecks@login-0.soperator-login-headless-svc.soperator.svc.cluster.local << 'EOF'
+    soperatorchecks@login-0.soperator-login-headless-svc.soperator.svc.cluster.local \
+    "flock /var/lock/apt.lock bash -s" << 'EOF'
 
 # Patterns of packages to process
 TARGET_PATTERNS=(
@@ -23,17 +24,17 @@ for pattern in "${TARGET_PATTERNS[@]}"; do
   for pkg in $pkgs; do
     if apt-mark showhold | grep -q "^$pkg$"; then
       echo "Unholding $pkg"
-      sudo apt-mark unhold -o DPkg::Lock::Timeout=-1 "$pkg"
+      sudo apt-mark unhold "$pkg"
     fi
   done
 done
 
 echo "Removing existing CUDA installation..."
-sudo apt remove -o DPkg::Lock::Timeout=-1 -y cuda || true
+sudo apt remove -y cuda || true
 
 echo "Installing CUDA version ${CUDA_VERSION}..."
-sudo apt -o DPkg::Lock::Timeout=-1 update
-sudo apt install -o DPkg::Lock::Timeout=-1 -y "cuda=${CUDA_VERSION}"
+sudo apt update
+sudo apt install -y "cuda=${CUDA_VERSION}"
 
 sudo apt clean
 sudo apt autoremove -y
@@ -44,9 +45,10 @@ for pattern in "${TARGET_PATTERNS[@]}"; do
   for pkg in $pkgs; do
     if dpkg -s "$pkg" &>/dev/null; then
       echo "Holding $pkg"
-      sudo apt-mark hold -o DPkg::Lock::Timeout=-1 "$pkg"
+      sudo apt-mark hold "$pkg"
     fi
   done
 done
 
 echo "CUDA ${CUDA_VERSION} installation complete and relevant packages pinned (held)."
+EOF
--- helm/soperator-activechecks/scripts/upgrade-health-checker.sh
+++ helm/soperator-activechecks/scripts/upgrade-health-checker.sh
@@ -2,11 +2,11 @@ set -e
 
 apt update && apt install -y retry
 
-echo "Installing libcudnn8"
+echo "Upgrading nc-health-checker to the version {{ .Values.upgradeHealthChecker.healthCheckerVersion }}"
 
 # If we have more than one login node and create user on one of them,
 # other node become unavailable for new SSH connections for around 10 seconds.
 retry -d 2 -t 10 -- ssh -i /mnt/jail/opt/soperator-home/soperatorchecks/.ssh/soperatorchecks_id_ecdsa \
     -o StrictHostKeyChecking=no \
     soperatorchecks@login-0.soperator-login-headless-svc.soperator.svc.cluster.local \
-    'flock /var/lock/apt.lock bash -c "sudo apt update && sudo apt install -y libcudnn8"'
+    'flock /var/lock/apt.lock bash -c "sudo apt update && sudo apt install --only-upgrade nc-health-checker={{ .Values.upgradeHealthChecker.healthCheckerVersion }}"'
