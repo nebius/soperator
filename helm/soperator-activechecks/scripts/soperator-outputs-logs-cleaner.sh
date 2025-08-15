49e3cf134230918f4d7d65b06beab4c8e60c8514
Pavel Sofronii <pavel.sofrony@nebius.com>
2025-08-15 09:11:33 +0200

    if last modified > 4 hours, and run the check every 2 hours

--- helm/soperator-activechecks/scripts/soperator-outputs-logs-cleaner.sh
+++ helm/soperator-activechecks/scripts/soperator-outputs-logs-cleaner.sh
@@ -1,5 +1,5 @@
 set -euxo pipefail
 
-echo "Cleaning old Soperator outputs (health check logs)"
+echo "Cleaning old Soperator outputs (health check logs) if last modify > 4 hours"
 
-chroot /mnt/jail /bin/sh -c "find /opt/soperator-outputs -type f -mtime +7 -delete"
+chroot /mnt/jail /bin/sh -c "find /opt/soperator-outputs -type f -mmin +240 -delete"
