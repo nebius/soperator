1d6f47a637491a4aef2a48eff6ce0892eac0d76f
Meena Kerolos <meena@nebius.com>
2025-08-19 15:15:06 +0200

    Add proper extensive check

--- helm/soperator-activechecks/scripts/extensive-check.sh
+++ helm/soperator-activechecks/scripts/extensive-check.sh
@@ -1,26 +1,78 @@
 #!/bin/bash
-#SBATCH --deadline="now+6hours"
-#SBATCH --time=10:00
-#SBATCH --mem=32G
-#SBATCH --gpus-per-node=1
-#SBATCH --cpus-per-task=2
+#SBATCH --time=01:00:00
+#SBATCH --gpus-per-node=8
 
-echo "Running extensive health check..."
+apt update
+apt install --only-upgrade nc-health-checker
+apt-cache policy nc-health-checker
 
-sleep 30
-
-# Always fail in order not to remove the reservation until we implement a real extensive check
-HEALTH_CHECK_PASSED=$((0))
-
-# For testing: use this line to randomly fail or succeed
-# HEALTH_CHECK_PASSED=$(($RANDOM % 2))
-
-echo "HEALTH_CHECK_PASSED=$HEALTH_CHECK_PASSED"
-
-if [[ $HEALTH_CHECK_PASSED -eq 1 ]]; then
-  echo "Health-checker passed or returned non-ERROR status."
+echo "Checking for running GPU processes..."
+if [[ -n "$(nvidia-smi --query-compute-apps=pid --format=csv,noheader | grep -v '^ *$')" ]]; then
+  echo "Another GPU process is currently running. Exiting."
   exit 0
-else
-  echo "Health-checker reported status=ERROR and exited with non-zero status."
-  exit 1
 fi
+
+platform=""
+gpus_on_node=$(nvidia-smi --query-gpu=name --format=csv,noheader | sort | uniq -c)
+if [[ "${gpus_on_node}" == *"8 NVIDIA H100"* ]]; then
+  platform="8xH100"
+elif [[ "${gpus_on_node}" == *"8 NVIDIA H200"* ]]; then
+  platform="8xH200"
+elif [[ "${gpus_on_node}" == *"8 NVIDIA B200"* ]]; then
+  platform="8xB200"
+else
+  echo "Unsupported platform"
+  exit 0
+fi
+echo "Platform found: $platform"
+
+
+# (1) I don't get why we use srun here. We are already on a worker node!
+# srun --cpu-bind=verbose,cores --gpus-per-node=8 bash -c "health-checker run -e soperator -p $platform -n all_reduce_without_ib --report-format json-pretty"
+
+# (2) Do we need --cpu-bind=verbose,cores for the following tests? and why?
+# - all_reduce_without_ib
+# - all_reduce_with_ib
+# - mem_bw
+# - mem_lat
+
+all_reduce_without_ib() {
+  health-checker run -e soperator -p $platform -n all_reduce_without_ib --report-format json-pretty
+}
+
+all_reduce_with_ib() {
+  health-checker run -e soperator -p $platform -n all_reduce_with_ib --report-format json-pretty
+}
+
+mem_bw() {
+  health-checker run -e soperator -p $platform -n mem_bw --report-format json-pretty
+}
+
+mem_lat() {
+  health-checker run -e soperator -p $platform -n mem_lat --report-format json-pretty
+}
+
+gpu_fryer() {
+  HC_GPU_FRYER_DURATION=120 health-checker run -e soperator -p $platform -n gpu_fryer --report-format json-pretty
+}
+
+funcs_to_test=(all_reduce_without_ib all_reduce_with_ib mem_bw mem_lat gpu_fryer)
+for test in "${funcs_to_test[@]}"
+do
+  echo "Running $test"
+  echo "on $(hostname)..."
+  HC_OUTPUT=$($test)
+  HC_EXIT_CODE=$?
+
+  echo "Health checker output: $HC_OUTPUT"
+  echo "Health checker exit code: $HC_EXIT_CODE"
+  HC_STATUS=$(echo "$HC_OUTPUT" | awk '/^\s*{/,/^\s*}/' | jq -r '.status')
+
+  echo "Health checker status: $HC_STATUS"
+  if [[ "$HC_STATUS" == "ERROR" && $HC_EXIT_CODE -eq 1 ]]; then
+    echo "Health-checker reported status=ERROR and exited with non-zero status."
+    exit 1 # Fail fast 
+  else
+    echo "Health-checker passed or returned non-ERROR status."
+  fi
+done
