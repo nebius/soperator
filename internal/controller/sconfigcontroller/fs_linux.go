fbbc27f1a12101d93762e7bde826f60b12758a2e
dnugmanov <40948799+dnugmanov@users.noreply.github.com>
2025-08-21 17:09:11 +0500

    sconfigcontroller: linux fallback to os.Rename with logs when renameat2 unsupported; add log on exchange failure in batch (#1452)

--- internal/controller/sconfigcontroller/fs_linux.go
+++ internal/controller/sconfigcontroller/fs_linux.go
@@ -2,12 +2,37 @@
 
 package sconfigcontroller
 
-import "golang.org/x/sys/unix"
+import (
+	"errors"
+	"os"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func isRenameNotSupported(err error) bool {
+	return errors.Is(err, syscall.EINVAL) || errors.Is(err, syscall.ENOTSUP) || errors.Is(err, syscall.ENOSYS)
+}
 
 func renameExchange(oldPath, newPath string) error {
-	return unix.Renameat2(unix.AT_FDCWD, oldPath, unix.AT_FDCWD, newPath, unix.RENAME_EXCHANGE)
+	if err := unix.Renameat2(unix.AT_FDCWD, oldPath, unix.AT_FDCWD, newPath, unix.RENAME_EXCHANGE); err != nil {
+		if isRenameNotSupported(err) {
+			log.Log.V(1).Info("renameat2 exchange not supported; falling back to os.Rename", "oldPath", oldPath, "newPath", newPath, "err", err)
+			return os.Rename(oldPath, newPath)
+		}
+		return err
+	}
+	return nil
 }
 
 func renameNoReplace(oldPath, newPath string) error {
-	return unix.Renameat2(unix.AT_FDCWD, oldPath, unix.AT_FDCWD, newPath, unix.RENAME_NOREPLACE)
+	if err := unix.Renameat2(unix.AT_FDCWD, oldPath, unix.AT_FDCWD, newPath, unix.RENAME_NOREPLACE); err != nil {
+		if isRenameNotSupported(err) {
+			log.Log.V(1).Info("renameat2 noreplace not supported; falling back to os.Rename", "oldPath", oldPath, "newPath", newPath, "err", err)
+			return os.Rename(oldPath, newPath)
+		}
+		return err
+	}
+	return nil
 }
