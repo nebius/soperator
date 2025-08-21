661402bdf8de8499fb75a926d18e7e3fb6d66294
Uburro <grigorii.rochev@nebius.com>
2025-08-21 18:21:59 +0200

    add list apps.kruise.io,resources=statefulsets

--- internal/controller/soperatorchecks/pod_ephemeral_storage_check.go
+++ internal/controller/soperatorchecks/pod_ephemeral_storage_check.go
@@ -33,6 +33,7 @@ import (
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 // +kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch
 // +kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=apps.kruise.io,resources=statefulsets,verbs=get;list;watch;
 
 var (
 	PodEphemeralStorageCheckName = "soperatorchecks.pod-ephemeral-storage-check"
