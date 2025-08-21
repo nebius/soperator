5da2270b7026005e0cf1bcaa0bd7e25cce59ac03
Meena Kerolos <meena@nebius.com>
2025-08-21 11:44:22 +0200

    specify 8 gpus

--- helm/soperator-activechecks/scripts/extensive-check.sh
+++ helm/soperator-activechecks/scripts/extensive-check.sh
@@ -2,6 +2,7 @@
 #SBATCH --deadline="now+24hours"
 #SBATCH --time=01:00:00
 #SBATCH --exclusive
+#SBATCH --gpus-per-node=8
 
 echo "Upgrading nc-health-checker to the version 1.0.0-147.250819"
 # We could use `retry -d 2 -t 10 --` here but it's not currently installed in jail.
@@ -55,12 +56,7 @@ mem_lat() {
        bash -c "health-checker run -e soperator -p $platform -n mem_lat --report-format json-pretty"
 }
 
-cuda_samples() {
-  srun --container-image="$ACTIVE_CHECKS_IMAGE" \
-        --container-mounts=$(which health-checker):/usr/local/bin/health-checker \
-        --cpu-bind=verbose \
-        bash -c "health-checker run -e soperator -p $platform -n deviceQuery,vectorAdd,simpleMultiGPU,p2pBandwidthLatencyTest --report-format json-pretty"
-}
+
 
 dcgmi_diag_r2() {
   health-checker run -e soperator -p $platform -n dcgmi_diag_r2 --report-format json-pretty
@@ -73,14 +69,21 @@ gpu_fryer() {
   bash -c "HC_GPU_FRYER_DURATION=300 health-checker run -e soperator -p $platform -n gpu_fryer --report-format json-pretty"
 }
 
+cuda_samples() {
+  srun --container-image="$ACTIVE_CHECKS_IMAGE" \
+        --container-mounts=$(which health-checker):/usr/local/bin/health-checker \
+        --cpu-bind=verbose \
+        bash -c "health-checker run -e soperator -p $platform -n deviceQuery,vectorAdd,simpleMultiGPU,p2pBandwidthLatencyTest --report-format json-pretty"
+}
+
 funcs_to_test=(
   all_reduce_without_ib
   all_reduce_with_ib
   mem_bw
   mem_lat
-  cuda_samples
   dcgmi_diag_r2
   gpu_fryer
+  cuda_samples
 )
 for test in "${funcs_to_test[@]}"
 do
