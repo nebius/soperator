15078f17d96f979d192ae83172f1a7482f443d03
Dmitry Starov <dmitry.staroff@gmail.com>
2025-10-27 15:01:55 +0300

    [ADD] Map function for slices

--- internal/utils/sliceutils/filter_test.go
+++ internal/utils/sliceutils/filter_test.go
@@ -8,31 +8,21 @@ import (
 	"nebius.ai/slurm-operator/internal/utils/sliceutils"
 )
 
-var (
-	filterTestCases = []TestCase{{
-		A: 10,
-		B: "hello",
-	}, {
-		A: 20,
-		B: "bye",
-	}}
-)
-
 func TestFilter(t *testing.T) {
 	t.Run("Test Filter found", func(t *testing.T) {
-		f := sliceutils.Filter(filterTestCases, func(t TestCase) bool {
+		f := sliceutils.Filter(testCases, func(t TestCase) bool {
 			return t.A == 10
 		})
 		assert.Equal(t, "hello", f[0].B)
 
-		f = sliceutils.Filter(filterTestCases, func(t TestCase) bool {
+		f = sliceutils.Filter(testCases, func(t TestCase) bool {
 			return t.B == "bye"
 		})
 		assert.Equal(t, 20, f[0].A)
 	})
 
 	t.Run("Test Filter not found", func(t *testing.T) {
-		f := sliceutils.Filter(filterTestCases, func(t TestCase) bool {
+		f := sliceutils.Filter(testCases, func(t TestCase) bool {
 			return t.A == 0
 		})
 		assert.Empty(t, f)
--- internal/utils/sliceutils/sliceutils_test.go
+++ internal/utils/sliceutils/sliceutils_test.go
@@ -4,3 +4,13 @@ type TestCase struct {
 	A int
 	B string
 }
+
+var (
+	testCases = []TestCase{{
+		A: 10,
+		B: "hello",
+	}, {
+		A: 20,
+		B: "bye",
+	}}
+)
