5aeaea3ea908eeccda9ef2cbae59d92f4e992f91
Dmitry Starov <dmitry.staroff@gmail.com>
2025-10-27 15:04:41 +0300

    [ADD] NodeSet lister

--- internal/consts/annotation.go
+++ internal/consts/annotation.go
@@ -7,4 +7,6 @@ const (
 	AnnotationDefaultContainerName = "kubectl.kubernetes.io/default-container"
 	AnnotationClusterName          = K8sGroupNameSoperator + "/cluster"
 	AnnotationActiveCheckName      = K8sGroupNameSoperator + "/activecheck"
+
+	AnnotationParentalClusterRefName = K8sGroupNameSoperator + "/parental-cluster-ref"
 )
