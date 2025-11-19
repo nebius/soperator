4dd1395409c9f8cc44b611441ac6ce2bea79dda4
Pavel Sofronii <pavel.sofrony@nebius.com>
2025-11-19 17:40:22 +0100

    fix: add bind mounts

--- images/k8s_check_job/k8s_check_job_entrypoint.sh
+++ images/k8s_check_job/k8s_check_job_entrypoint.sh
@@ -2,6 +2,18 @@
 
 set -euxo pipefail # Exit immediately if any command returns a non-zero error code
 
+echo "Bind-mount virtual filesystems"
+mount -t proc /proc proc/
+mount -t sysfs /sys sys/
+mount --rbind /dev dev/
+mount --rbind /run run/
+
+echo "Bind-mount cgroup filesystem"
+mount --rbind /sys/fs/cgroup sys/fs/cgroup
+
+echo "Bind-mount /tmp"
+mount --bind /tmp tmp/
+
 echo "Bind-mount DNS configuration"
 mount --bind /etc/resolv.conf /mnt/jail/etc/resolv.conf
 
