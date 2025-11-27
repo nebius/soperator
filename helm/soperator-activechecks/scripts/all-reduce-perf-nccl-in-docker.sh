29c65792f13d28790a1104aff6087ec5f15f9ad0
Mikhail Mokrushin <rodrijjke@nebius.com>
2025-11-27 16:59:00 +0100

    Preinstall sudo in active check images

--- helm/soperator-activechecks/scripts/all-reduce-perf-nccl-in-docker.sh
+++ helm/soperator-activechecks/scripts/all-reduce-perf-nccl-in-docker.sh
@@ -5,16 +5,15 @@
 #SBATCH --exclusive
 #SBATCH --mem=0
 
-
 echo "Running all_reduce_perf_nccl_in_docker check on $(hostname)..."
 
-mkdir -p /tmp/soperatorchecks/a
-mkdir -p /tmp/soperatorchecks/b
+mkdir -p /tmp/soperatorchecks/docker_check/a
+mkdir -p /tmp/soperatorchecks/docker_check/b
 
-srun --gpus=8 docker run --rm \
+srun docker run --rm \
   --gpus=all --device=/dev/infiniband \
-  -v /tmp/soperatorchecks/a:/a \
-  --mount type=bind,source=/tmp/soperatorchecks/b,target=/b \
+  -v /tmp/soperatorchecks/docker_check/a:/a \
+  --mount type=bind,source=/tmp/soperatorchecks/docker_check/b,target=/b \
   {{ include "activecheck.image.docker" . }} \
   bash -l -c "NCCL_P2P_DISABLE=1 NCCL_SHM_DISABLE=1 NCCL_ALGO=Ring all_reduce_perf -b 512M -e 8G -f 2 -g 8"
 
