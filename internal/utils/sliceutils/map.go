82fc0733091719c4504f11c3c60c69b679d49f45
Dmitry Starov <dmitry.staroff@gmail.com>
2025-11-29 17:01:44 +0100

    [ADD] Implement iterator functions for slice mapping

--- internal/utils/sliceutils/map.go
+++ internal/utils/sliceutils/map.go
@@ -1,10 +1,39 @@
 package sliceutils
 
-// Map applies f to each alement of the slice, and returns new slice containing processed elements.
+import (
+	"iter"
+)
+
+// Map applies f to each element of the slice, and returns new slice containing processed elements.
+// Deprecated: use MapSlice instead.
 func Map[T any, U any](slice []T, f func(T) U) []U {
 	result := make([]U, len(slice))
 	for i := 0; i < len(slice); i++ {
 		result[i] = f(slice[i])
 	}
 	return result
+}
+
+// MapSlice applies f to each element of the slice, and returns new slice containing processed elements.
+func MapSlice[T any, U any](slice []T, f func(T) U) []U {
+	return Collect(MapSeq(SliceSeq(slice), f))
+}
+
+// MapSliceSeq applies f to each element of the slice, and returns new slice containing processed elements.
+func MapSliceSeq[T any, U any](slice []T, f func(T) U) iter.Seq[U] {
+	return MapSeq(SliceSeq(slice), f)
+}
+
+// MapSeqSlice applies f to each element of the slice, and returns new slice containing processed elements.
+func MapSeqSlice[T any, U any](seq iter.Seq[T], f func(T) U) []U {
+	return Collect(MapSeq(seq, f))
+}
+
+// MapSeq applies f to each element of the slice, and returns new slice containing processed elements.
+func MapSeq[T any, U any](seq iter.Seq[T], f func(T) U) iter.Seq[U] {
+	return func(yield func(U) bool) {
+		seq(func(v T) bool {
+			return yield(f(v))
+		})
+	}
 }
--- internal/utils/sliceutils/map_test.go
+++ internal/utils/sliceutils/map_test.go
@@ -10,6 +10,13 @@ import (
 )
 
 func TestMap(t *testing.T) {
+	t.Run("Test Map empty", func(t *testing.T) {
+		f := sliceutils.Map(emptyTestCases, func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Empty(t, f)
+	})
+
 	t.Run("Test Map string", func(t *testing.T) {
 		f := sliceutils.Map(testCases, func(t TestCase) string {
 			return fmt.Sprintf("%d x %s", t.A, t.B)
@@ -33,5 +40,141 @@ func TestMap(t *testing.T) {
 		assert.Equal(t, 40, f[1].Foo)
 		assert.Equal(t, "hellohello", f[0].Bar)
 		assert.Equal(t, "byebye", f[1].Bar)
+	})
+}
+
+func TestMapSlice(t *testing.T) {
+	t.Run("Test MapSlice empty", func(t *testing.T) {
+		f := sliceutils.MapSlice(emptyTestCases, func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Empty(t, f)
+	})
+
+	t.Run("Test MapSlice string", func(t *testing.T) {
+		f := sliceutils.MapSlice(testCases, func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Equal(t, "10 x hello", f[0])
+		assert.Equal(t, "20 x bye", f[1])
+	})
+
+	t.Run("Test MapSlice custom type", func(t *testing.T) {
+		type custom struct {
+			Foo int
+			Bar string
+		}
+		f := sliceutils.MapSlice(testCases, func(t TestCase) custom {
+			return custom{
+				Foo: t.A * 2,
+				Bar: t.B + t.B,
+			}
+		})
+		assert.Equal(t, 20, f[0].Foo)
+		assert.Equal(t, 40, f[1].Foo)
+		assert.Equal(t, "hellohello", f[0].Bar)
+		assert.Equal(t, "byebye", f[1].Bar)
+	})
+}
+
+func TestMapSliceSeq(t *testing.T) {
+	t.Run("Test MapSliceSeq empty", func(t *testing.T) {
+		f := sliceutils.MapSliceSeq(emptyTestCases, func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Empty(t, sliceutils.Collect(f))
+	})
+
+	t.Run("Test MapSliceSeq string", func(t *testing.T) {
+		f := sliceutils.MapSliceSeq(testCases, func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Equal(t, "10 x hello", sliceutils.Collect(f)[0])
+		assert.Equal(t, "20 x bye", sliceutils.Collect(f)[1])
+	})
+
+	t.Run("Test MapSliceSeq custom type", func(t *testing.T) {
+		type custom struct {
+			Foo int
+			Bar string
+		}
+		f := sliceutils.MapSliceSeq(testCases, func(t TestCase) custom {
+			return custom{
+				Foo: t.A * 2,
+				Bar: t.B + t.B,
+			}
+		})
+		assert.Equal(t, 20, sliceutils.Collect(f)[0].Foo)
+		assert.Equal(t, 40, sliceutils.Collect(f)[1].Foo)
+		assert.Equal(t, "hellohello", sliceutils.Collect(f)[0].Bar)
+		assert.Equal(t, "byebye", sliceutils.Collect(f)[1].Bar)
+	})
+}
+
+func TestMapSeqSlice(t *testing.T) {
+	t.Run("Test MapSeqSlice empty", func(t *testing.T) {
+		f := sliceutils.MapSeqSlice(sliceutils.SliceSeq(emptyTestCases), func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Empty(t, f)
+	})
+
+	t.Run("Test MapSeqSlice string", func(t *testing.T) {
+		f := sliceutils.MapSeqSlice(sliceutils.SliceSeq(testCases), func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Equal(t, "10 x hello", f[0])
+		assert.Equal(t, "20 x bye", f[1])
+	})
+
+	t.Run("Test MapSeqSlice custom type", func(t *testing.T) {
+		type custom struct {
+			Foo int
+			Bar string
+		}
+		f := sliceutils.MapSeqSlice(sliceutils.SliceSeq(testCases), func(t TestCase) custom {
+			return custom{
+				Foo: t.A * 2,
+				Bar: t.B + t.B,
+			}
+		})
+		assert.Equal(t, 20, f[0].Foo)
+		assert.Equal(t, 40, f[1].Foo)
+		assert.Equal(t, "hellohello", f[0].Bar)
+		assert.Equal(t, "byebye", f[1].Bar)
+	})
+}
+
+func TestMapSeq(t *testing.T) {
+	t.Run("Test MapSeq empty", func(t *testing.T) {
+		f := sliceutils.MapSeq(sliceutils.SliceSeq(emptyTestCases), func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Empty(t, sliceutils.Collect(f))
+	})
+
+	t.Run("Test MapSeq string", func(t *testing.T) {
+		f := sliceutils.MapSeq(sliceutils.SliceSeq(testCases), func(t TestCase) string {
+			return fmt.Sprintf("%d x %s", t.A, t.B)
+		})
+		assert.Equal(t, "10 x hello", sliceutils.Collect(f)[0])
+		assert.Equal(t, "20 x bye", sliceutils.Collect(f)[1])
+	})
+
+	t.Run("Test MapSeq custom type", func(t *testing.T) {
+		type custom struct {
+			Foo int
+			Bar string
+		}
+		f := sliceutils.MapSeq(sliceutils.SliceSeq(testCases), func(t TestCase) custom {
+			return custom{
+				Foo: t.A * 2,
+				Bar: t.B + t.B,
+			}
+		})
+		assert.Equal(t, 20, sliceutils.Collect(f)[0].Foo)
+		assert.Equal(t, 40, sliceutils.Collect(f)[1].Foo)
+		assert.Equal(t, "hellohello", sliceutils.Collect(f)[0].Bar)
+		assert.Equal(t, "byebye", sliceutils.Collect(f)[1].Bar)
 	})
 }
