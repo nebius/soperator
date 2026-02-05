6a7402d45c5c2a8a643ea780ca8e37adddbde10d
Mikhail Mokrushin <rodrijjke@nebius.com>
2026-02-05 20:17:24 +0100

    Remove empty lib bind-mount targets in populate_jail

--- images/populate_jail/populate_jail_entrypoint.sh
+++ images/populate_jail/populate_jail_entrypoint.sh
@@ -23,6 +23,22 @@ populate_jail_rootfs() {
     ssh-keygen -t ecdsa -f /mnt/jail/root/.ssh/id_ecdsa -N "" && cat /mnt/jail/root/.ssh/id_ecdsa.pub >> /mnt/jail/root/.ssh/authorized_keys
 }
 
+remove_empty_lib_mount_targets() {
+    echo "Removing the flag file that shows that GPU library bind-mount targets exist"
+    rm "/mnt/jail/etc/gpu_libs_installed.flag"
+
+    echo "Removing empty library files that were used as bind-mount targets on the previous cluster"
+    ARCH_LIST="x86_64 aarch64"
+    for arch in $ARCH_LIST; do
+        find "/mnt/jail/lib/${arch}-linux-gnu" \
+            -maxdepth 1 -type f ! -type l -empty -print |
+        while IFS= read -r file; do
+            echo "Removing $file"
+            rm -- "$file"
+        done
+    done
+}
+
 if [ "${OVERWRITE:-}" = "1" ]; then
     echo "Content overwriting is turned on, repopulating jail directory"
     populate_jail_rootfs
@@ -32,7 +48,8 @@ else
         echo "Jail directory is empty, populating"
         populate_jail_rootfs
     elif [ -d /mnt/jail/dev ] || [ -d /mnt/jail/etc ] || [ -d /mnt/jail/usr ]; then
-        echo "Jail directory is already populated with something that resembles a rootfs, exiting"
+        echo "Jail directory is already populated with something that resembles a rootfs, removing empty libs"
+        remove_empty_lib_mount_targets
         exit 0
     else
         echo "Jail directory is filled with something that does not resemble a rootfs, failing"
