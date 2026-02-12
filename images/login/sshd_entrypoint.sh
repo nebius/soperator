466100a5907c10ec07a68b560485b82200020599
Dzmitry Amialiusik <amialiusik@nebius.com>
2026-02-12 20:45:15 +0100

    fix long termination of worker and login pods

--- images/login/sshd_entrypoint.sh
+++ images/login/sshd_entrypoint.sh
@@ -33,4 +33,4 @@ echo "Waiting until munge started"
 while [ ! -S "/run/munge/munge.socket.2" ]; do sleep 2; done
 
 echo "Start sshd daemon"
-/usr/sbin/sshd -D -e -f /mnt/ssh-configs/sshd_config
+exec /usr/sbin/sshd -D -e -f /mnt/ssh-configs/sshd_config
