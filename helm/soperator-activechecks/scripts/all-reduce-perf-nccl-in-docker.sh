4b098836d301fddf2a619509bb05edd98bea673a
Pavel Sofronii <pavel.sofrony@nebius.com>
2026-02-12 16:13:11 +0100

    SCHED-952 Fix active check for docker

--- helm/soperator-activechecks/scripts/all-reduce-perf-nccl-in-docker.sh
+++ helm/soperator-activechecks/scripts/all-reduce-perf-nccl-in-docker.sh
@@ -14,6 +14,7 @@ srun docker run --rm \
   --gpus=all --device=/dev/infiniband \
   -v /tmp/soperatorchecks/docker_check/a:/a \
   --mount type=bind,source=/tmp/soperatorchecks/docker_check/b,target=/b \
+  -e NVIDIA_DISABLE_REQUIRE=1 \
   {{ include "activecheck.image.docker" . }} \
   bash -l -c "NCCL_P2P_DISABLE=1 NCCL_SHM_DISABLE=1 NCCL_ALGO=Ring all_reduce_perf -b 512M -e 8G -f 2 -g 8"
 
