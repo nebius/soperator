9b1053471b1a586a151b42d88795921b690d39b9
Nikita Sokolov <faucct@nebius.com>
2026-03-19 15:33:49 +0100

    groom task_prolog

--- helm/slurm-cluster/slurm_scripts/task_prolog.sh
+++ helm/slurm-cluster/slurm_scripts/task_prolog.sh
@@ -2,19 +2,23 @@
 
 set -euxo pipefail
 
-export CHECKS_OUTPUTS_BASE_DIR="/opt/soperator-outputs"
-task_prolog="$CHECKS_OUTPUTS_BASE_DIR/task_prolog"
-(umask 000; mkdir -p "$task_prolog")
-(
-    printf "Slurm environment variables for task prolog:\n"
-    env | grep -E '^(SLURM_|SLURMD_|SRUN_|SBATCH_)' || true
-    printf "\n"
-) > "$task_prolog/$SLURMD_NODENAME.$SLURM_JOB_ID.${SLURM_STEP_ID:-}.${SLURM_ARRAY_TASK_ID:-}.out"
+# Runs once at the job beginning without `SLURM_STEP_ID` and for each step with `SLURM_STEP_ID`.
 
-if [ -v SOPERATOR_CHECKS_RUNNER_CPU_BIND ] && [ "${SOPERATOR_CHECKS_RUNNER_CPU_BIND:-}" = "${SLURM_CPU_BIND:-}" ]; then
-    echo print "A recursive srun was detected. Unset SOPERATOR_CHECKS_RUNNER_CPU_BIND, if you think this is fine.">&2
+if [ "${SLURM_LOCALID}" = "0" ]; then
+    export CHECKS_OUTPUTS_BASE_DIR="/opt/soperator-outputs"
+    task_prolog="$CHECKS_OUTPUTS_BASE_DIR/task_prolog"
+    (umask 000; mkdir -p "$task_prolog")
+    (
+        printf "Slurm environment variables for task prolog:\n"
+        env | grep -E '^(SLURM_|SLURMD_|SRUN_|SBATCH_)' || true
+        printf "\n"
+    ) > "$task_prolog/$SLURMD_NODENAME.$SLURM_JOB_ID.${SLURM_STEP_ID:-}.${SLURM_ARRAY_TASK_ID:-}.out"
 fi
 
-if [ -v SLURM_CPU_BIND ]; then
-    echo export "SOPERATOR_CHECKS_RUNNER_CPU_BIND=${SLURM_CPU_BIND:-}"
+if [ -v SLURM_JOB_ID_SOPERATOR_TASK_PROLOG ] && [ "${SLURM_JOB_ID_SOPERATOR_TASK_PROLOG:-}" != "${SLURM_JOB_ID:-}" ] && [ -z "${SOPERATOR_SURPRESS_RECURSIVE_SRUN:-}" ]; then
+    echo print "A Job from another Job was detected. Set SOPERATOR_SURPRESS_RECURSIVE_SRUN, if you think this is fine.">&2
+fi
+
+if [ -v SLURM_JOB_ID ]; then
+    echo export "SLURM_JOB_ID_SOPERATOR_TASK_PROLOG=${SLURM_JOB_ID:-}"
 fi
