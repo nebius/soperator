c205b9e917d21ca2675856658a41c47ac67eff3c
Nikita Sokolov <faucct@nebius.com>
2026-03-19 11:57:39 +0100

    task_prolog.sh should group logs by SLURM_ARRAY_TASK_ID

--- helm/slurm-cluster/slurm_scripts/task_prolog.sh
+++ helm/slurm-cluster/slurm_scripts/task_prolog.sh
@@ -3,12 +3,13 @@
 set -euxo pipefail
 
 export CHECKS_OUTPUTS_BASE_DIR="/opt/soperator-outputs"
-task_prolog="/mnt/jail$CHECKS_OUTPUTS_BASE_DIR/task_prolog"
+task_prolog="$CHECKS_OUTPUTS_BASE_DIR/task_prolog"
 (umask 000; mkdir -p "$task_prolog")
 (
-    printf "SLURM_CPU_BIND=%q\n" "${SLURM_CPU_BIND:-}"
-    printf "SOPERATOR_CHECKS_RUNNER_CPU_BIND=%q\n" "${SOPERATOR_CHECKS_RUNNER_CPU_BIND:-}"
-) >> "$task_prolog/$SLURMD_NODENAME.$SLURM_JOB_ID.$SLURM_STEP_ID.out"
+    printf "Slurm environment variables for task prolog:\n"
+    env | grep -E '^(SLURM_|SLURMD_|SRUN_|SBATCH_)' || true
+    printf "\n"
+) > "$task_prolog/$SLURMD_NODENAME.$SLURM_JOB_ID.$SLURM_STEP_ID${SLURM_ARRAY_TASK_ID:+".$SLURM_ARRAY_TASK_ID"}.out"
 
 if [ -v SOPERATOR_CHECKS_RUNNER_CPU_BIND ] && [ "${SOPERATOR_CHECKS_RUNNER_CPU_BIND:-}" = "${SLURM_CPU_BIND:-}" ]; then
     echo print "A recursive srun was detected. Unset SOPERATOR_CHECKS_RUNNER_CPU_BIND, if you think this is fine.">&2
