3dfecad25af3fc702a36560b304f4f1b2f805114
Nikita Sokolov <faucct@nebius.com>
2026-03-19 14:58:45 +0100

    task prolog should not fail when running before steps

--- helm/slurm-cluster/slurm_scripts/task_prolog.sh
+++ helm/slurm-cluster/slurm_scripts/task_prolog.sh
@@ -9,7 +9,7 @@ task_prolog="$CHECKS_OUTPUTS_BASE_DIR/task_prolog"
     printf "Slurm environment variables for task prolog:\n"
     env | grep -E '^(SLURM_|SLURMD_|SRUN_|SBATCH_)' || true
     printf "\n"
-) > "$task_prolog/$SLURMD_NODENAME.$SLURM_JOB_ID.$SLURM_STEP_ID${SLURM_ARRAY_TASK_ID:+".$SLURM_ARRAY_TASK_ID"}.out"
+) > "$task_prolog/$SLURMD_NODENAME.$SLURM_JOB_ID.${SLURM_STEP_ID:-}.${SLURM_ARRAY_TASK_ID:-}.out"
 
 if [ -v SOPERATOR_CHECKS_RUNNER_CPU_BIND ] && [ "${SOPERATOR_CHECKS_RUNNER_CPU_BIND:-}" = "${SLURM_CPU_BIND:-}" ]; then
     echo print "A recursive srun was detected. Unset SOPERATOR_CHECKS_RUNNER_CPU_BIND, if you think this is fine.">&2
