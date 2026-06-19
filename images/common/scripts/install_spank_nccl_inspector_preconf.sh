ac03dd4e293c7341296c6309e1fc072f1f97aed4
Dmitry Starov <dmitry.staroff@gmail.com>
2026-06-19 15:53:30 +0200

    [REF] Install NCCL Inspector PreConf plugin via Ansible

--- images/common/scripts/install_spank_nccl_inspector_preconf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e # Exit immediately if any command returns a non-zero error code
-
-apt-get update
-apt-get -y install --reinstall spank-nccl-inspector-preconf
-apt-get clean
-rm -rf /var/lib/apt/lists/*
