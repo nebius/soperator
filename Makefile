82cf2d3af355a997a3993d708b6a6ad192560169
Fabrizio Waldner <fabrizio@nebius.com>
2026-07-17 17:10:59 +0200

    SCHED-1967: Add Slurm-cluster user initialization hook

--- Makefile
+++ Makefile
@@ -278,6 +278,7 @@ sync-version: yq ## Sync versions from file
 	@$(YQ) -i ".images.populateJailTag = \"$(IMAGE_VERSION)\"" "helm/slurm-cluster/values.yaml"
 	@$(YQ) -i ".images.soperatorExporter = \"$(IMAGE_REPO)/soperator-exporter:$(IMAGE_VERSION)\"" "helm/slurm-cluster/values.yaml"
 	@$(YQ) -i ".images.sConfigController = \"$(IMAGE_REPO)/sconfigcontroller:$(OPERATOR_IMAGE_TAG)\"" "helm/slurm-cluster/values.yaml"
+	@$(YQ) -i ".images.k8sJob = \"$(IMAGE_REPO)/k8s_check_job:$(IMAGE_VERSION)\"" "helm/slurm-cluster/values.yaml"
 	@$(YQ) -i ".images.mariaDB = \"docker-registry1.mariadb.com/library/mariadb:12.1.2\"" "helm/slurm-cluster/values.yaml"
 	@# endregion helm/slurm-cluster/values.yaml
 
---
+++ helm/slurm-cluster/scripts/create-user-soperatorchecks.sh
@@ -0,0 +1,21 @@
+set -ex
+
+echo "Creating soperatorchecks user..."
+chroot /mnt/jail /bin/bash -s <<'EOF'
+
+id "soperatorchecks" || echo "" | soperator-createuser soperatorchecks --home /opt/soperator-home/soperatorchecks --gecos ""
+
+if [ ! -f /opt/soperator-home/soperatorchecks/.ssh/soperatorchecks_id_ecdsa ]; then
+  echo "Generating ssh key..."
+  ssh-keygen -t ecdsa -f /opt/soperator-home/soperatorchecks/.ssh/soperatorchecks_id_ecdsa -N '' -C soperatorchecks
+  cat /opt/soperator-home/soperatorchecks/.ssh/soperatorchecks_id_ecdsa.pub >> /opt/soperator-home/soperatorchecks/.ssh/authorized_keys
+fi
+
+mkdir -p /etc/soperatorchecks
+chown soperatorchecks:soperatorchecks /etc/soperatorchecks
+
+EOF
+
+# Because of the bug in filestore ssh is unavailable for ~15 sec after new user creation.
+echo "Wait for ssh availability 20 sec..."
+sleep 20
