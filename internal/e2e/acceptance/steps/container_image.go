f38aa9d8025cce338d108a53b909456d843dbd0d
Pavel Sofronii <pavel.sofrony@nebius.com>
2026-07-21 14:10:32 +0200

    skip enroot e2e

--- internal/e2e/acceptance/steps/container_image.go
+++ internal/e2e/acceptance/steps/container_image.go
@@ -1,6 +1,6 @@
 package steps
 
 const (
-	e2eNCCLContainerImageDocker = "cr.eu-north1.nebius.cloud/ml-containers/training_diag:12.9.0-ubuntu24.04-20260624090429"
-	e2eNCCLContainerImageEnroot = "docker://cr.eu-north1.nebius.cloud#ml-containers/training_diag:12.9.0-ubuntu24.04-20260624090429"
+	e2eNCCLContainerImageDocker = "cr.eu-north1.nebius.cloud/ml-containers/training_diag:13.0.2-ubuntu24.04-20260709140028"
+	e2eNCCLContainerImageEnroot = "docker://cr.eu-north1.nebius.cloud#ml-containers/training_diag:13.0.2-ubuntu24.04-20260709140028"
 )
